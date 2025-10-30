1f27645a3b3dd2e69e08f953bba6c4845fc676f2
MSilva505 <ftsoffline606@gmail.com>
2025-10-30 15:50:31 -0400

    atualizacao no repos java

--- java-developer/estudos-etc/udemy-ex-projects/src/exerciocioestatico/applicacao/Programa.java
+++ java-developer/estudos-etc/udemy-ex-projects/src/exerciocioestatico/applicacao/Programa.java
@@ -8,12 +8,14 @@ import java.util.Scanner;
 public class Programa {
 
     public static void main(String args[]) {
+       
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in); 
         System.out.println("Qual o preco do dolar?");
         System.out.println("Qual sera as quantidades dos dolars ?");
         double precoDoDolar = sc.nextDouble();
         double qtdDosDolars =sc.nextDouble();
+        double precoFinal = DolarParaReal.pagamentoFinal();
         DolarParaReal.multiplicacaoDosDolars(precoDoDolar, qtdDosDolars);
         DolarParaReal.taxaDaIof();
         System.out.println("voce tera que pague isso (taxa da iof ja incluso)");
